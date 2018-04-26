312904e532b4b5138eb11447d3e67ae03d422670
LiamPollock <37652526+LiamPollock@users.noreply.github.com>
2018-04-26 23:23:52 +0930

    Create navbar.js

---
+++ Milestone 2/navbar.js
@@ -0,0 +1,11 @@
+//*Function based on this video, https://www.youtube.com/watch?v=rUjiGOct808*//
+
+function navigation() {
+  var navbar = document.getElementById('nav-bar');
+  if(navbar.style.display == "block") { 
+    navbar.style.display = "none";
+  }
+  else {
+    navbar.style.display = "block";
+  }
+}
